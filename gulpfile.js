45ffdb36b2a407b6f6b20aabe47174b49b8f7c79
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-19 14:12:10 +0000

    linter auto-fix added for js

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('test', function (cb) {
 
 gulp.task('lint', function() {
     var filesToLint = [
-        '**/*.{html,js}',
+        '**/*.js',
         '!tests/protractor.conf.js',
         '!dist/**/*',
         '!docs/**/*',
@@ -41,6 +41,8 @@ gulp.task('lint', function() {
     return gulp.src(filesToLint)
             .pipe(debug({title: 'eslint (Scope):'}))
             .pipe(eslint())
+            .pipe(eslint({fix:true}))
             .pipe(eslint.format())
-            .pipe(eslint.failAfterError());
+            .pipe(gulp.dest('.'));
+            //.pipe(eslint.failAfterError());
 });
